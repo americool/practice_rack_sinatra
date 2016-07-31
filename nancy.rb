4e2a714ab4148121c207aa7b6e8e0ccf75db8cd7
Americool <aberanderson@gmail.com>
2016-07-31 13:39:19 -0400

    working on nancy

---
+++ nancy.rb
@@ -0,0 +1,44 @@
+#nancy.rb
+require "rack"
+
+module Nancy
+  class Base
+
+    def initialize
+      @routes = {}
+    end
+
+    attr_reader :routes
+
+    def get(path, &handler)
+      route("GET", path, &handler)
+    end
+
+    def call(env)
+      @request = Rack::Request.new(env)
+      verb = @request.request_method
+      requested_path = @request.path_info
+
+      handler = @routes[verb][requested_path]
+
+      handler.call
+    end
+
+    private
+
+    def route(verb, path, &handler)
+      @routes[verb] ||= {}
+      @routes[verb][path] = handler
+    end
+  end
+end
+
+nancy = Nancy::Base.new
+
+nancy.get "/hello" do
+  [200, {}, ["Nancy says hello"]]
+end
+
+#puts nancy.routes
+
+Rack::Handler::WEBrick.run nancy, Port:9292
